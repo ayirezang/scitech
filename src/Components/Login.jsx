35a7af5fb2bbb63772c7fa32939b2085569b9876
ayirezang <patienceayirezang33@gmail.com>
2025-12-04 18:12:22 +0000

    login

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import upworkLogo from "../assets/upworkLogo.png";
 import googleLogo from "../assets/googleLogo.png";
+import { FaEyeSlash } from "react-icons/fa";
 import { FaApple } from "react-icons/fa";
+import { IoChevronDown } from "react-icons/io5";
+import { FaUpwork } from "react-icons/fa6";
 const Login = () => {
   return (
     <div className="bg-white min-h-screen">
-      <nav>
+      {/**navbar */}
+      <nav className="">
         <div className="flex justify-between items-center px-4 py-4">
           {/**upwork logo */}
           <div className="">
@@ -20,7 +24,7 @@ const Login = () => {
       </nav>
       {/**main section */}
       <main className="p-8 items-center">
-        <div className=" w-full max-w-2xl mx-auto ">
+        <div className=" w-full max-w-2xl mx-auto  ">
           {/**header */}
           <div>
             <p className="text-center  text-4xl mb-8">
@@ -56,8 +60,12 @@ const Login = () => {
           <form>
             <div className="flex gap-2 ">
               {/**first name */}
-              <div>
-                <label for="first name" className="block">
+              <div className="flex-1">
+                <label
+                  htmlFor="first name"
+                  for="first name"
+                  className="block mb-1"
+                >
                   First name
                 </label>
                 <input
@@ -65,25 +73,144 @@ const Login = () => {
                   name="first name"
                   id="first name"
                   className="border px-5 py-3 rounded-lg w-full outline-none border-gray-600"
+                  placeholder="Leonard"
                 />
               </div>
               {/**last name */}
-              <div>
-                <label for="first name" className="block">
-                  First name
+              <div className="flex-1">
+                <label htmlFor="first name" className="block mb-1">
+                  Last name
                 </label>
                 <input
                   type="text"
                   name="first name"
                   id="first name"
-                  className="border px-5 py-3 rounded-lg w-full outline-none border-gray-600"
+                  className="border px-5 py-3 rounded-lg w-full outline-none border-gray-600  text-black"
+                  placeholder="Kim"
                 />
+              </div>
+            </div>
+            {/**email */}
+            <div className="my-4">
+              <label for="email" className="block mb-1">
+                Email
+              </label>
+              <input
+                type="email"
+                name="email"
+                id="email"
+                className="border px-5 py-3 rounded-lg w-full outline-none border-gray-600 text-black"
+                placeholder="pat@gmail.com"
+              />
+            </div>
+            <div className="my-4">
+              <label for="password" className="block mb-1">
+                Password
+              </label>
+              <div className="relative">
+                <input
+                  type="password"
+                  name="password"
+                  id="password"
+                  className="border px-5 py-3 rounded-lg w-full outline-none border-gray-600 text-black"
+                  placeholder="********"
+                />
+                <div className="absolute right-4 top-1/2 -translate-y-1/2 cursor-pointer">
+                  <FaEyeSlash />
+                </div>
+              </div>
+              {/**country */}
+              <div className="my-4">
+                <label for="country" className="block mb-1">
+                  Country
+                </label>
+                <div className="relative">
+                  <input
+                    type="text"
+                    name="country"
+                    id="country"
+                    className="border px-5 py-3 rounded-lg w-full outline-none border-gray-600"
+                    placeholder="United States"
+                  />
+                  <div className="absolute right-4 top-1/2 -translate-y-1/2 cursor-pointer">
+                    <IoChevronDown />
+                  </div>
+                </div>
+              </div>
+              {/**checkbox 1 */}
+              <div className="flex flex-col gap-5 mt-10">
+                <div className="flex items-start gap-1">
+                  <input
+                    type="checkbox"
+                    name="terms"
+                    id="terms"
+                    className="w-5 h-5 text-green-600"
+                  />
+                  <label htmlFor="terms" className="ml-2 cursor-pointer">
+                    Send me helpful emails to find rewarding work and job leads
+                  </label>
+                </div>
+
+                {/**checkbox 2 */}
+                <div className="flex items-start gap-1">
+                  <input
+                    type="checkbox"
+                    name="terms"
+                    id="terms2"
+                    className="w-5 h-5 text-green-600 flex shrink-0"
+                  />
+                  <label htmlFor="terms" className="ml-2 cursor-pointer">
+                    Yes, I understand and agree to the{" "}
+                    <span className="text-green-600 underline">
+                      Upwork Terms of Service,
+                    </span>{" "}
+                    including the{" "}
+                    <span className="text-green-600 underline">
+                      User Agreement
+                    </span>{" "}
+                    and{" "}
+                    <span className="text-green-600 underline">
+                      Privacy Policy
+                    </span>
+                    .
+                  </label>
+                </div>
+              </div>
+              {/**create account button */}
+              <div className="mt-10 flex justify-center">
+                <button className="bg-green-600 text-white px-10 py-2 rounded-lg">
+                  Create my account
+                </button>
+              </div>
+              <div className="mt-5 text-center">
+                <p className="">
+                  Already have an account?{" "}
+                  <span className="text-green-600">Log In</span>
+                </p>
               </div>
             </div>
           </form>
         </div>
       </main>
-      <footer></footer>
+      {/**footer */}
+      <footer>
+        <div className="bg-black w-full flex justify-between px-10 py-5 ">
+          {/**upwork */}
+          <div className="flex gap-2">
+            <div className="bg-white rounded-md flex justify-center items-center w-8  h-7 ">
+              <FaUpwork />
+            </div>
+            <p className="text-white">Upwork</p>
+          </div>
+          {/** */}
+          <div>
+            <p className="text-white ">
+              curated by
+              <span className="font-bold text-2xl"> Mobbin</span>
+            </p>
+          </div>
+        </div>
+      </footer>
     </div>
   );
 };
